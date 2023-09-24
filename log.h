5e4bdf88237229c6475d19018fb6c04892ab69e8
heylarissa <larissah.dandrade@gmail.com>
2023-09-23 23:41:51 -0300

    adicionando estrutra

--- log.h
+++ log.h
@@ -11,8 +11,12 @@ typedef struct
 } log;
 
 #define ATAQUES_FILE "R_ATAQUES.txt"
+#define ENTIDADES_FILE "R_ENTIDADES.txt"
+
+#define TRUE 1
+#define FALSE 0
 void get_ataques(atributo *dados, int quantidade, FILE *arquivo);
-void get_entidades(atributo *dados, int quantidade);
+void get_entidades(atributo *dados, int quantidade, FILE *arquivo);
 void get_tamanho(atributo *dados, int quantidade);
 void get_firewall(atributo *dados, int quantidade);
 
